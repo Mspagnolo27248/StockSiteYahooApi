0aaeb39060c29298a5cd499caa970b9052aed1c2
Michael Spagnolo <mspagnolo27@gmail.com>
2022-05-04 12:23:45 -0400

    update exdate

--- public/content/options.js
+++ public/content/options.js
@@ -20,7 +20,7 @@ function dateDiff(a, b) {
 class BlackScholes {
     static callPrice(stockPrice, impliedVol, exPrice, exDate, rfRate = .0060, ) {
         const today = new Date()
-        const exerciseDate = new Date(exDate)
+        const exerciseDate = new Date(new Date(exDate).getTime()+(990*60000))
         const yearsToExpiration = dateDiff(exerciseDate, today) / 365
         const pvExPrice = exPrice * Math.exp(((-1 * rfRate )*yearsToExpiration))
         const d0 = impliedVol * (yearsToExpiration ** .5)
